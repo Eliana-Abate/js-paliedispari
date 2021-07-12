d59334f874abc5efb2c9e448fb6b55acaa25ac8e
Eliana-Abate <eliana.abate@outlook.it>
2021-07-12 15:55:36 +0200

    palindrome check

--- Palindroma/js/script.js
+++ Palindroma/js/script.js
@@ -4,6 +4,9 @@ Creare una funzione per capire se la parola inserita è palindroma */
 
 userWord = checkPalindrome();
 
+var result = isPalindrome(userWord);
+console.log(userWord + 'è palindroma?' + return);
+
 
 function checkPalindrome (word) {
     word = prompt('Inserisci una parola');
@@ -12,3 +15,17 @@ function checkPalindrome (word) {
 }
 
 
+
+
+function isPalindrome(emme) {
+    isPalindrome = false;
+
+    var em = Math.floor(emme.length / 2);
+
+    for (var i = 0; i < em; i++)
+      if (emme[i] === emme.length - i - 1)
+      return true;
+  } 
+
+
+ 
