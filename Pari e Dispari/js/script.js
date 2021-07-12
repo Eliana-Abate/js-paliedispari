5ebfa87c04f48ad8d8d4feabc8bca5c0a5402687
Eliana-Abate <eliana.abate@outlook.it>
2021-07-12 17:32:23 +0200

    added function for getting user choices

--- Pari e Dispari/js/script.js
+++ Pari e Dispari/js/script.js
@@ -5,3 +5,34 @@ Sommiamo i due numeri
 Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
 Dichiariamo chi ha vinto. */ 
 
+var choice = evenOddChoice();
+
+var number = getNumber();
+
+
+function evenOddChoice(userChoice) {
+    
+    userChoice = prompt('Scegli: pari o dispari?').toLowerCase();
+    
+    while (userChoice != 'pari' && userChoice != 'dispari') {
+        userChoice = prompt('Scegli: pari o dispari?').toLowerCase();
+    }
+
+    console.log('L\'utente ha scelto: ' + userChoice); 
+    return userChoice;
+  }
+
+
+
+
+function getNumber(userNumber) {
+    
+    userNumber = prompt('Inserisci un numero da 1 a 5');
+
+    while (isNaN(userNumber) || userNumber < 1 || userNumber > 5) {
+      userNumber = prompt('Inserisci un numero da 1 a 5');
+    }
+  
+    console.log('Il numero scelto dall\'utente è: ' + userNumber);
+    return userNumber;
+  }
