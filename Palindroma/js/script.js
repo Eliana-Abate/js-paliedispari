6dac1940435e2e5810d496c8a6a5acb688cc5070
Eliana-Abate <eliana.abate@outlook.it>
2021-07-12 16:55:54 +0200

    added toLowerCase for a right validation + PRINT IN HTML

--- Palindroma/js/script.js
+++ Palindroma/js/script.js
@@ -2,15 +2,23 @@
 Chiedere all’utente di inserire una parola
 Creare una funzione per capire se la parola inserita è palindroma */
 
+var displayUserWord = document.getElementById('user-word');
+
+var displayPalindromeCheck = document.getElementById('palindrome-check');
+
+
+
 var userWord = checkPalindrome();
 console.log(userWord);
+displayUserWord.innerText = 'L\'utente ha scritto: ' + userWord;
 
 var result = isPalindrome(userWord);
 console.log(userWord + ' è palindroma? ' + result);
 
 
+
 function checkPalindrome (word) {
-    word = prompt('Inserisci una parola');
+    word = prompt('Inserisci una parola').toLocaleLowerCase();
 
     return word;
 }
@@ -25,10 +33,12 @@ function isPalindrome(emme) {
     for (var i = 0; i < em; i++) {
 
       if (emme[i] !== emme[emme.length - i - 1]){
-      return false;
+        displayPalindromeCheck.innerText = 'La parola scelta dall\'utente non è palindroma';
+        return false;
       } 
 
     } 
+    displayPalindromeCheck.innerText = 'La parola scelta dall\'utente è palindroma';
     return true;
   } 
 
