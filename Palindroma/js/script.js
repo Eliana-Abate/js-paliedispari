49d5d28a0d4f623ac019eaf4f388c41d6d213403
Eliana-Abate <eliana.abate@outlook.it>
2021-07-12 15:32:37 +0200

    Es. Palindorma: start

---
+++ Palindroma/js/script.js
@@ -0,0 +1,14 @@
+/* Palidroma
+Chiedere all’utente di inserire una parola
+Creare una funzione per capire se la parola inserita è palindroma */
+
+userWord = checkPalindrome();
+
+
+function checkPalindrome (word) {
+    word = prompt('Inserisci una parola');
+
+    return (word);
+}
+
+
